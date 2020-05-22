b89386a66d013e11ebff8abb36c35e87c2dc15d7
Harika Paluri <harikapaluri@gmail.com>
2020-05-21 19:43:52 -0500

    Commit from local Desktop

---
+++ Integration/src/main/java/com/example/integration/KafkaProducer.java
@@ -0,0 +1,17 @@
+/*
+ * package com.example.integration;
+ * 
+ * import java.util.Properties;
+ * 
+ * import org.springframework.beans.factory.annotation.Value;
+ * 
+ * public class KafkaProducer {
+ * 
+ * @Value("${kafka.bootstrap.servers}") private String kafkaBootstrapServers;
+ * 
+ * Defining producer properties.
+ * 
+ * Properties producerProperties = new Properties();
+ * 
+ * }
+ */
